f09dd164e07aebd9de15ba2124c772b00b9a2ae2
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-31 14:56:26 -0600

    Set up SCSS for project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter as Router } from 'react-router-dom';
 import rootReducer from './reducers';
 import App from './components/App/App';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, composeWithDevTools());
 
-const store = createStore(rootReducer, composedWithDevTools())
 
 ReactDOM.render(
-<Provider>
   <Router>
-    <App />
-  </Router>
-</Provider>,
-document.getElementById('root'));
+    <Provider
+      store={store}
+    >
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
