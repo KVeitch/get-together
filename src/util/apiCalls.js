cefe1e2c17db282b00e5a554fdebd1b5b2300b77
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-02 16:33:35 -0600

    Add fetch calls for Flights and images

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -1,5 +1,4 @@
- import apiKey from './apiKey';
-
+ import { apiKey, unsplashApiKey }  from './apiKey';
 
 export const getFlights = async (date, startingLocation, destination) => {
   const flightSearchURL = 'https://apidojo-hipmunk-v1.p.rapidapi.com/flights/create-session?country=US';
@@ -12,17 +11,39 @@ export const getFlights = async (date, startingLocation, destination) => {
     }
   }
 
-  const results = await fetch(flightSearchURL+query, options)
-  return results.json()
+  const results = await fetch(flightSearchURL+query, options);
+  const data = await results.json();
+  const cleanedData = cleanFlightData(data);
+
+  return cleanedData
 }
 
-const cleanFlightData = (data) => ({
+const cleanFlightData = (data) => {
+  const legs = data.legs 
+  const cleanData = Object.keys(legs).map(leg => ({ 
+    depart:legs[leg].depart_iso,
+    arrive:legs[leg].arrive_iso,
+    from:legs[leg].from_code,
+    to:legs[leg].to_code,
+    flightNo:legs[leg].marketing_num[1]
+  }))
+  return cleanData
+}
 
-})
+export const getPhotos = async (location) => {
+  const unsplashBaseUrl ='https://api.unsplash.com/search/photos?';
+  const id = `client_id=${unsplashApiKey}`
+  const search = `query=${location}`
+  const orientation = 'orientation=landscape'
+  const perPage = 'per_page=8'
+  const url = `${unsplashBaseUrl}${id}&${search}&${orientation}&${perPage}&accept%20version=v1`
+  console.log(url)
+  const results = await fetch(url)
+  const data = await results.json()
+  const cleanData = cleanPhotoData(data.results)
+  return cleanData;
+}
 
-
-
-
-
-export const getPictures = () => {}
-
+const cleanPhotoData = (photos) => {
+  return  photos.map(photo => photo.urls.small)
+}
