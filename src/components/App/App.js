90f4e7ad53069a0d0e3eb0c5688e3f32d141a87d
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-02 17:04:10 -0600

    Add link to right image in header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,37 +9,42 @@ import Trip from '../Trip/Trip';
 import MainForm from '../MainForm/MainForm';
 import bluePlane from '../../images/blue-plane.png'
 
-export class App extends Component {
-  async componentDidMount() {
+// export class App extends Component {
+//   async componentDidMount() {
 
-  }
+//   }
 
-  render = () => (
+//   render = () => (
+
+const App = () => (
       <div className="App">
          <header className="title">
            <div className="title-container">
             <Link to="/">
-            <img className="plane-image" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
+              <img className="plane-image" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
             </Link>
             <Link to="/">
               <h1>Connection</h1>
             </Link>
-            <img className="plane-image image-mirror" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
+            <Link to="/">
+              <img className="plane-image image-mirror" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
+            </Link>
             </div>
         </header>
         <Route exact path="/" render={()=> <MainForm />}/>
         <Route exact path="/trip" render={() => <Trip /> }/>
       </div>
     );
+// };
+
+export default App;
+
+// export const mapStateToProps = (state) => ({
+
+// });
+
+// export const mapDispatchToProps = (dispatch) => (bindActionCreators({
   
-};
+// }, dispatch));
 
-export const mapStateToProps = (state) => ({
-
-});
-
-export const mapDispatchToProps = (dispatch) => (bindActionCreators({
-  
-}, dispatch));
-
-export default connect(mapStateToProps, mapDispatchToProps) (App);
+// export default connect(mapStateToProps, mapDispatchToProps) (App);
