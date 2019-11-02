0e8996a91e82a66da12d72a9954e80905a658ed6
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-02 16:37:13 -0600

    Add images reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux';
+import cityName from './cityName';
+import destination from './destination';
+import images from './images';
 import meFlights from './meFlights';
 import meStart from './meStart';
-import returnDate from './returnDate'
-import startDate from './startDate'
-import youStart from './youStart'
-import youFlights from './youFlights'
-import destination from './destination'
+import returnDate from './returnDate';
+import startDate from './startDate';
+import youFlights from './youFlights';
+import youStart from './youStart';
 
 const rootReducer = combineReducers({
+  cityName,
   destination,
+  images,
   meFlights,
   meStart,
   returnDate,
