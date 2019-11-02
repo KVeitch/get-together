e010e07f3db9b0dd9136a0e387637bcfcd58d618
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-02 16:38:41 -0600

    Corrects single double quotes in JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Route, Link} from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './App.scss';
 import Trip from '../Trip/Trip';
-import Main from '../Main/Main';
+import MainForm from '../MainForm/MainForm';
 import bluePlane from '../../images/blue-plane.png'
 
 export class App extends Component {
@@ -17,18 +17,18 @@ export class App extends Component {
   render = () => (
       <div className="App">
          <header className="title">
-           <div className='title-container'>
-            <Link to='/'>
-            <img className='plane-image' src={bluePlane} />
+           <div className="title-container">
+            <Link to="/">
+            <img className="plane-image" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
             </Link>
-            <Link to='/'>
+            <Link to="/">
               <h1>Connection</h1>
             </Link>
-            <img className='plane-image image-mirror' src={bluePlane} />
+            <img className="plane-image image-mirror" alt="Line art of a blue plane flying into a cloud" src={bluePlane} />
             </div>
         </header>
-        <Route exact path='/' render={()=> <Main />}/>
-        <Route exact path='/trip' render={() => <Trip /> }/>
+        <Route exact path="/" render={()=> <MainForm />}/>
+        <Route exact path="/trip" render={() => <Trip /> }/>
       </div>
     );
   
