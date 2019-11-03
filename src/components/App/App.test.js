f3ccb3295532ec895b39a6869a939f8095f37bba
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-03 16:12:10 -0700

    Removes unused import

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App', () => {
-  const wrapper = shallow(<App />);
-
+  
   it('should match the snapshot with all the data passed in correctly', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
